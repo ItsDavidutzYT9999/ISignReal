59ed5b1a0e91a1f7f1a66f5e328edaf42b03b094
ItsDavidutzYT9999 <prodavidutz@gmail.com>
2025-08-07 12:14:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from app import app
-from flask import render_template
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    """Render the main page with upload form"""
+def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
